60586cc5b941fe1cd7862c9942ba4a40578ea224
Damian Galetto <damian@prixmates.com>
2014-05-31 16:39:33 -0300

    Nueva Estructura
    Estructura para memoria v 0.5.0

---
+++ src/plugin/memoria.js
@@ -0,0 +1,4 @@
+/*!
+ * Memoria v0.5.0
+ * Docs: https://github.com/init-sfw/memoria
+ */
--- src/scripts/jquery/jquery-dialog-override.js
+++
@@ -1,9 +0,0 @@
-$.widget("ui.dialog", $.extend({}, $.ui.dialog.prototype, {
-    _title: function(title) {
-        if (!this.options.title ) {
-            title.html("&#160;");
-        } else {
-            title.html(this.options.title);
-        }
-    }
-}));
